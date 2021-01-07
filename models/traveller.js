f416b9eece984fc0c3c9f07f09488f187e67015c
Evelyn Szarek <evelynszarek@gmail.com>
2021-01-07 22:28:45 +0000

    One commit to rule'em'all

---
+++ models/traveller.js
@@ -0,0 +1,44 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map( (journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map( (journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter( (journey) => {
+    return journey.transport === transport;
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter( (journey) => {
+    return journey.distance >= 1000;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce( (total, journey) => {
+    return total += journey.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let uniqueList = this.journeys.map( (journey) => {
+    return journey.transport;
+  });
+  return [...new Set(uniqueList)];
+};
+
+
+module.exports = Traveller;
+
